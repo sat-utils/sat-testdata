05ae268acb1403b643e66d93f48951622060b8d4
Matthew Hanson <matt.a.hanson@gmail.com>
2016-04-21 12:50:19 -0400

    testdata class

---
+++ stestdata/testdata.py
@@ -0,0 +1,31 @@
+
+import os
+import glob
+
+
+class TestData(object):
+
+    path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'samples'))
+
+    def __init__(self, sensor):
+        """ Initialize testdata for a sensor """
+        self.sensor = sensor
+        # load bandmap
+
+    @classmethod
+    def list_sensors(cls):
+        """ List all sensors available """
+        return [f for f in os.listdir(cls.path) if os.path.isdir(f)]
+
+    def spath(self):
+        return os.path.join(self.path, self.sensor)
+
+    def list_scenes(self):
+        """ List scenes for this sensor """
+        return [f for f in os.listdir(self.spath()) if os.path.isdir(f)]
+
+    def get_filenames(self, scene):
+        """ Get dictionary of {filename: [bandnames]} for this sensor and scene """
+        spath = os.path.join(self.spath(), scene)
+        fnames = glob.glob(os.path.join(spath), '*')
+        # make dictionary
