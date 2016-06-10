d559dd176eed2b229052242f425b1df957c80925
Matthew Hanson <matt.a.hanson@gmail.com>
2016-06-10 15:14:27 -0400

    fix test

--- tests/test_data.py
+++ tests/test_data.py
@@ -16,4 +16,4 @@ class Test(unittest.TestCase):
         t = TestData('landsat8')
         self.assertTrue(isinstance(t.examples, dict))
         self.assertTrue('small_full_data_cloudy' in t.examples)
-        self.assertEquals(t.examples['small_full_data_cloudy']['B1']['filename'], 'test_B1.tif')
+        self.assertEquals(t.examples['small_full_data_cloudy']['B1']['filename'], 'l8_B1.tif')
